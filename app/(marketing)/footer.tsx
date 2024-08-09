087136a19b6449e5fa943fd29d69c5985ec4af34
Yahav Gal <yahav526@gmail.com>
2024-08-09 11:55:12 +0300

    Styling changes

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,39 @@
+/* Imports */
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
+/* Assets */
+import hr from "../../public/hr.svg";
+import es from "../../public/es.svg";
+import fr from "../../public/fr.svg";
+import it from "../../public/it.svg";
+import jp from "../../public/jp.svg";
+
 export const Footer = () => {
     return (
         <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 px-2">
-            FOOTER
+            <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src={hr} alt="Croatian" height={32} width={40} className="mr-4 rounded-md" />
+                    Croatian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src={es} alt="Croatian" height={32} width={40} className="mr-4 rounded-md" />
+                    Spanish
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src={fr} alt="Croatian" height={32} width={40} className="mr-4 rounded-md" />
+                    French
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src={it} alt="Croatian" height={32} width={40} className="mr-4 rounded-md" />
+                    Italian
+                </Button>
+                <Button size="lg" variant="ghost" className="w-full">
+                    <Image src={jp} alt="Croatian" height={32} width={40} className="mr-4 rounded-md" />
+                    Japanese
+                </Button>
+            </div>
         </footer>
     )
 }
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -12,7 +12,9 @@ import Link from "next/link";
 /* Exports */
 export default function Home() {
   return (
-    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2" style={{
+      background: "radial-gradient(circle, rgba(16,185,75,0.5) 0%, transparent 70%)"
+    }}>
       <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
         <Image src={hero} alt="hero"/>
       </div>
@@ -20,7 +22,7 @@ export default function Home() {
         <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
           Learn, practice and master new languages with Lango.
         </h1>
-        <div>
+        <div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
           <ClerkLoading>
             <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
           </ClerkLoading>
