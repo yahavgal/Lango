b0d063879a177d57fadcd188bd1841af086bae37
Yahav Gal <yahav526@gmail.com>
2024-08-17 08:56:11 +0300

    Added database seed scripts

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
+import { courses } from "@/database/schema";
 
-/* Assets */
 
 
 type Props = {
-    activeCourse: { title: string, imageSource: string } //TODO
+    activeCourse: typeof courses.$inferSelect;
     hearts: number;
     points: number;
     hasActiveSubscription: boolean;
@@ -19,7 +19,7 @@ export const UserProgress = ({activeCourse, points, hearts, hasActiveSubscriptio
         <div className="flex items-center justify-between gap-x-2 w-full">
             <Link href="/courses">
                 <Button variant="ghost">
-                    <Image src={activeCourse.imageSource} alt={activeCourse.title} className="rounded-md border" width={32} height={32} />
+                    <Image src={activeCourse.imageSrc} alt={activeCourse.title} className="rounded-md border" width={32} height={32} />
                 </Button>
             </Link>
             <Link href="/shop">
